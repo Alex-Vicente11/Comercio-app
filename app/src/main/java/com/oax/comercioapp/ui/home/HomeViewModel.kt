e9c7a3169f0f760273e47ad01592064faf325e9c
Alex-Vicente11 <teiker_01@hotmail.com>
2025-09-01 02:36:52 -0600

    Actualizar crear producto /HomeViewModel.kt

--- app/src/main/java/com/oax/comercioapp/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/oax/comercioapp/ui/home/HomeViewModel.kt
@@ -6,15 +6,20 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.oax.comercioapp.data.api.NetworkResult
 import com.oax.comercioapp.data.models.Product
+import com.oax.comercioapp.data.models.ProductRequest
+import com.oax.comercioapp.data.models.ProductResponse
 import com.oax.comercioapp.data.repository.ProductRepository
 import kotlinx.coroutines.launch
 
-class HomeViewModel : ViewModel() {
+class HomeViewModel(private val productRepository: ProductRepository = ProductRepository()) : ViewModel() {
 
-  private val productRepository = ProductRepository()
+  //private val productRepository = ProductRepository()
   
   private val _products = MutableLiveData<NetworkResult<List<Product>>>()
   val products: LiveData<NetworkResult<List<Product>>> = _products
+
+  private val _createProductResult = MutableLiveData<NetworkResult<ProductResponse>>()
+  val createProductResult: LiveData<NetworkResult<ProductResponse>> = _createProductResult
   
   private val _text = MutableLiveData<String>().apply {
     value = "Productos"
@@ -31,6 +36,14 @@ class HomeViewModel : ViewModel() {
         _products.postValue(result)
       }
     }
+  }
+
+  fun createProduct(productRequest: ProductRequest){
+      viewModelScope.launch {
+          productRepository.createProduct(productRequest).collect { result ->
+              _createProductResult.postValue(result)
+          }
+      }
   }
   
   fun refreshProducts() {
