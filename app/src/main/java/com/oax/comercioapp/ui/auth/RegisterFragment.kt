86d959d17c8b1c141f6aebd5dab05f6b5fb136e6
Alex-Vicente11 <teiker_01@hotmail.com>
2025-12-11 12:54:38 -0600

    registerFragment inflater

--- app/src/main/java/com/oax/comercioapp/ui/auth/RegisterFragment.kt
+++ app/src/main/java/com/oax/comercioapp/ui/auth/RegisterFragment.kt
@@ -1,4 +1,26 @@
 package com.oax.comercioapp.ui.auth
 
-class RegisterFragment {
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.oax.comercioapp.databinding.FragmentRegisterBinding
+
+class RegisterFragment: stFragment(){
+    private var _binding: FragmentRegisterBinding? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ) = FragmentRegisterBinding.inflate(inflater, container, false).also {
+        _binding = it
+    }.root
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
